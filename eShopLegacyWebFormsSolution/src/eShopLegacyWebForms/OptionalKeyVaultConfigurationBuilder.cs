0e38f10c13ee4169e6991206ac8517c5e3699c5d
Fola Adeleke-Adedoyin <folaa@microsoft.com>
2019-07-15 16:04:49 -0400

    added configuration builder

---
+++ eShopLegacyWebFormsSolution/src/eShopLegacyWebForms/OptionalKeyVaultConfigurationBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Configuration.ConfigurationBuilders;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace eShopLegacyWebForms
+{
+    /// <summary> 
+    /// A version of <see cref="AzureKeyVaultConfigBuilder"/> that correctly handles optional. See https://github.com/aspnet/MicrosoftConfigurationBuilders/pull/56 
+    /// for general fix. 
+    /// </summary> 
+    public class OptionalKeyVaultConfigurationBuilder : AzureKeyVaultConfigBuilder
+    {
+        protected override void LazyInitialize(string name, NameValueCollection config)
+        {
+            try
+            {
+                base.LazyInitialize(name, config);
+            }
+            catch (Exception) when (Optional)
+            {
+            }
+        }
+    }
+
+}
